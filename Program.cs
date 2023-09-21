b09f1d15dd2938b49d769ac61781ff1c39a9d97f
Vishal github <Vishal.kunchal@gmail.com>
2023-09-21 17:58:03 +0530

    [vishal kunchal]: Created two line for eqality

--- Program.cs
+++ Program.cs
@@ -13,11 +13,39 @@
             Console.WriteLine("enter the value of y1");
             double y1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("enter the value of y1");
+            Console.WriteLine("enter the value of y2");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            double length_of_line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine(length_of_line);
+            double length_of_line_1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine(length_of_line_1);
+
+            Console.WriteLine("enter the value of a1");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the value of a2");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the value of b1");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the value of b2");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+
+            double length_of_line_2 = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            Console.WriteLine(length_of_line_2);
+
+            Console.WriteLine(length_of_line_2 == length_of_line_1);
+
+            if (length_of_line_1 == length_of_line_2)
+            {
+                Console.WriteLine("two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("two lines are not equal");
+            }
+
+
         }
     }
 }
